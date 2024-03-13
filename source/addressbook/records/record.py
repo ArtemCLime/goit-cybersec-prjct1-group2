4fd3d4c674f240e46b9fc21b2c37cfa3feda8b2b
Mike Gavrylov <gavrikster@gmail.com>
2024-03-13 23:49:57 +0200

    initial record implementation

--- source/addressbook/records/record.py
+++ source/addressbook/records/record.py
@@ -12,32 +12,46 @@ class Record:
 
         Attributes:
             name: Name, The name of the record.
-            phone: Phone, a list of phone numbers of the record.
-            email: Email, The email of the record.
+            phones: Phones, a list of phone numbers of the record.
+            emails: Emails, a list of email of the record.
             address: Address, The address of the record.
             birthday: Birthday, The birthdate of the record.
 
     """
-    def __init__(self, name: str, email: Optional[str] = None, phones: Optional[List[str]] = None, address: Optional[str] = None, birthday: Optional[str] = None):
+    def __init__(self, name: str, 
+                 emails: Optional[List[str]] = None, 
+                 phones: Optional[List[str]] = None, 
+                 address: Optional[str] = None, 
+                 birthday: Optional[str] = None):
         self.name = Name(name)
         self.phones = [Phone(phone) for phone in phones] if phones else []
-        self.email = Email(email) if email else None
+        self.emails = [Email(email) for email in emails] if emails else []
         self.address = Address(address) if address else None
         self.birthday = Birthday(birthday) if birthday else None
 
 
     def __str__(self):
-        return f'{self.name} {self.phone} {self.email}'
+        return '{name} | {phones} | {emails}'.format(
+            name = self.name,
+            phones = "; ".join(self.phones),
+            emails = "; ".join(self.emails) 
+        )
     
     def to_json(self):
-        """ Return a JSON representation of the record. """
-        pass
-        # TODO: Implement the to_json method.
+        """ Return a JSONable dictionary of the record. """
+        return {
+            "phones": [str(p) for p in self.phones],
+            "emails": [str(e) for e in self.emails],
+            "address": str(self.address),
+            "birthday": str(self.birthday)
+        }
     
     def from_json(self, json: Union[str, dict]):
-        """ Set the record from a JSON representation. """
-        pass
-        # TODO: Implement the from_json method.
+        """ Set the record from a JSONable dictionary. """
+        self.phones = [Phone(phone) for phone in json["phones"]] if json["phones"] else []
+        self.emails = [Email(email) for email in json["emails"]] if json["emails"] else [] 
+        self.address = Address(json["address"]) if json["address"] else None
+        self.birthday = Birthday(json["birthday"]) if json["birthday"] else None 
     
     # CRUD operations, Create, Read, Update, Delete.
     # TODO Implement the CRUD operations.
@@ -57,3 +71,6 @@ class Record:
     def delete(self, field_name: str):
         """ Delete a field from the record. """
         pass
+
+if __name__ == "__main__":
+    record = Record('test')

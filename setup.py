59336440d9850d000482342e306b85ed60384a2a
Mike Gavrylov <gavrikster@gmail.com>
2024-03-16 00:05:52 +0200

    setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
-from setuptools import setup, find_namespace_packages
+from setuptools import setup, find_packages
 
-setup(name='bot_assistant',
-      version='1',
-      description='Address book bot assistant',
-      url='https://github.com/ArtemCLime/goit-cybersec-prjct1-group2',
-      author='DataDefenders',
-      license='MIT',
-      packages=find_namespace_packages(),
-      install_requires=['rich', 'pytest'],
-      entry_points={'console_scripts': ['bot-assistant = bot_assistant.source:main']}
+setup(
+    name='datadefendersbot',
+    version='1.0',  
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "rich",
+        "pytest"
+    ],
+    entry_points={
+        'console_scripts': [
+            'datadefendersbot=source.main:main',  
+        ],
+    },
 )
